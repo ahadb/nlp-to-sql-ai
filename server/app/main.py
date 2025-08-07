89fba560255d8ac6973662a0e62fead821814045
ahadb <ahadbokhari@gmail.com>
2025-08-07 11:13:49 -0500

    fix db folder tracking and add files

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,4 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import os
 from fastapi import FastAPI, HTTPException, File, UploadFile, Query
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
